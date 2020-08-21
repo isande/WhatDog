7ea2d00c650441e28e45c8985eca22e8ac2bfab0
Peggy Wollenhaupt <isande@gmail.com>
2020-08-20 18:29:38 -0600

    initial commit

---
+++ WhatDog/View Controllers/PhotoSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoSelectViewController.swift
+//  WhatDog
+//
+//  Created by Peggy Wollenhaupt on 8/20/20.
+//  Copyright © 2020 Peggy Wollenhaupt. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
